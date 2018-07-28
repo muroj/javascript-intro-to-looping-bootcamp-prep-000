ab137c7a59fb64a67eca88a63bfd990cf33c84fe
Joe Muro <jmuro0321@gmail.com>
2018-07-28 15:42:43 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,4 +1,13 @@
 function forLoop(a) {
   
-  for (let )
+  for (let i = 0; i < 25; i++) {
+    
+    if (i !== 1) {
+      a.push("I am ${i} strange loops.");
+    } else {
+      a.push("I am ${i} strange loop.");
+    }
+  }
+  
+  return a;
 }
